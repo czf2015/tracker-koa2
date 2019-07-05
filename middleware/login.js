f7cad6870cff7bb6f8c3e4f6ecb4f82141d1d9e0
czf2015 <122385930@qq.com>
2019-07-05 17:29:54 +0800

    ~ bug

--- middleware/login.js
+++ middleware/login.js
@@ -2,19 +2,19 @@ const passport = require('./passport.js')
 
 module.exports = (ctx, next) => {
   // 必须 return
-  return passport.authenticate('local', (err, status, info) => {
+  return passport.authenticate('local', (err, user, info) => {
     if (err) {
       ctx.body = {
         code: -1,
         msg: err
       }
     } else {
-      if (status) {
+      if (user) {
         ctx.body = {
           code: 0,
           msg: '登录成功',
         }
-        return ctx.login(status)
+        return ctx.login(user)
       } else {
         ctx.body = {
           code: 1,
